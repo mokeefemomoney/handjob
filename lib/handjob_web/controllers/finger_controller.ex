7f63496928dace97f628adfe89f7c7c321f513f0
mokeefemomoney <mokeefemomoney@gmail.com>
2025-09-24 15:30:14 -0700

    steps

--- lib/handjob_web/controllers/finger_controller.ex
+++ lib/handjob_web/controllers/finger_controller.ex
@@ -1,8 +1,39 @@
 defmodule HandjobWeb.FingerController do
   use HandjobWeb, :controller
 
+  alias Circuits.GPIO
+
+  @step_pin Application.compile_env(:handjob_web, :step_pin, "GPIO15")
+  @dir_pin Application.compile_env(:handjob_web, :dir_pin, "GPIO14")
+  @enable_pin Application.compile_env(:handjob_web, :enable_pin, "GPIO18")
+
   def bang(conn, _params) do
-    # GPIO Stuff
+    {:ok, enable} = GPIO.open(@enable_pin, :output)
+    GPIO.write(enable, 0)
+
+    {:ok, direction} = GPIO.open(@dir_pin, :output)
+
+    {:ok, step} = GPIO.open(@step_pin, :output)
+
+    GPIO.write(direction, 0)
+
+    1..100
+    |> Enum.each(fn _ ->
+      GPIO.write(step, 1)
+      Process.sleep(1)
+      GPIO.write(step, 0)
+    end)
+
+    GPIO.write(direction, 1)
+
+    1..100
+    |> Enum.each(fn _ ->
+      GPIO.write(step, 1)
+      Process.sleep(1)
+      GPIO.write(step, 0)
+    end)
+
+    GPIO.write(enable, 1)
 
     json(conn, %{finger: "banged"})
   end
